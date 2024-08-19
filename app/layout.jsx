ff9e4701b1896c692d9274906186905209d2bef5
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-19 21:57:03 +0300

    layout and basic page done

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover and Share AI prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
